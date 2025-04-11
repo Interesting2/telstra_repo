d9b4093ec2b930988efdb27d5b17568841cb28b3
MetaCoderX <softwareguyhuron@gmail.com>
2025-04-11 23:32:28 +1000

    Added entities and repositoris and get request to find id from database

--- src/main/java/au/com/telstra/simcardactivator/controllers/SimActivationController.java
+++ src/main/java/au/com/telstra/simcardactivator/controllers/SimActivationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,11 +13,13 @@ import org.springframework.http.ResponseEntity;
 
 import au.com.telstra.simcardactivator.dto.SimActivationRequest;
 import au.com.telstra.simcardactivator.dto.SimActivationResponse;
+import au.com.telstra.simcardactivator.entities.SimCard;
 import reactor.core.publisher.Mono;
-
 import org.springframework.web.reactive.function.client.WebClient;
 
+import au.com.telstra.simcardactivator.repositories.SimCardRepository;
 
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/sim")
@@ -25,6 +28,21 @@ public class SimActivationController {
     @Autowired
     private WebClient.Builder webClientBuilder;
 
+    @Autowired
+    private SimCardRepository simCardRepository;
+
+    @GetMapping("/query")
+    public ResponseEntity<SimCard> getSimCardRecordById(@RequestParam Long simCardId) {
+        Optional<SimCard> matchedSimCardRecord = simCardRepository.findById(simCardId);
+
+        if (matchedSimCardRecord.isPresent()) {
+            SimCard simCard = matchedSimCardRecord.get();
+            return ResponseEntity.ok(simCard);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        
+        
+    }
 
     @PostMapping("/activate-sim")
     public void activateSim(@RequestBody SimActivationRequest request) {
@@ -42,11 +60,20 @@ public class SimActivationController {
 
         // shoot a request and wait for the result
         response.subscribe(responseBody -> {
-            if (responseBody.isSuccess()) {
+            boolean success = responseBody.isSuccess();
+
+            if (success) {
                 System.out.println("SIM card activation successful.");
             } else {
                 System.out.println("SIM card activation failed.");
             }
+            SimCard newSimCardRecord = new SimCard(
+                request.getIccid(),
+                request.getCustomerEmail(),
+                success
+            );
+            simCardRepository.save(newSimCardRecord); // save it to database
+            System.out.println("Saved new sim card record to database");
         }, error -> {
             System.out.println("Error while contacting the actuator: " + error.getMessage());
         });
